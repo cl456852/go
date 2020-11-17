015622f44b3b2d7743abdcf2fc8002c784fd3c82
chenlong10 <chenlong10@vipkid.com.cn>
2020-11-17 18:03:54 +0800

    test addr

--- GoTest.go
+++ GoTest.go
@@ -1,13 +1,32 @@
 package main
 
-import "unsafe"
+import "fmt"
 
-const (
-	a = "abc"
-	b = len(a)
-	c = unsafe.Sizeof(a)
-)
+type Data struct {
+	x int
+}
+
+func (self Data) ValueTest() { // 值接受者方法
+	fmt.Printf("值接收者: %p\n", &self)
+}
+func (self *Data) PointerTest() { // 指针接受者方法
+	fmt.Printf("指针接受者: %p\n", self)
+}
+
+type User struct {
+	name string
+	age  int
+}
 
 func main() {
-	println(a, b, c)
+	ma := make(map[int]*User)
+	andes := User{
+		name: "andes",
+		age:  19,
+	}
+	ma[1] = &andes
+	andes.age = 20
+
+	fmt.Println(ma[1].age)
+	fmt.Println(andes.age)
 }
